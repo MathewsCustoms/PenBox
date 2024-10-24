21352d8e9ca55ce89d6e6b521207dc9959126ad3
Mathews Customs <140462492+MathewsCustoms@users.noreply.github.com>
2024-10-24 16:08:43 -0400

    Fake Hack With Progress Bar

--- PowerShell Scripts/FakeHack.ps1
+++ PowerShell Scripts/FakeHack.ps1
@@ -1,32 +1,41 @@
 # Fake Progress Bar with Changing Operation
 $target = Read-Host 'Choose Your Target!'
-Write-Host "Starting Hack On $target..."
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" -ForegroundColor DarkCyan
+Write-Host "         Starting Hack On $target..." -ForegroundColor Yellow
+Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" -ForegroundColor DarkCyan
+Write-Host ""
 
 for ($i = 0; $i -le 100; $i++) {
 
     # Change the activity message based on ranges of percentages
-    if ($i -lt 5) {
+    if ($i -lt 3) {
         $activity = "Retrieving $target IP Address..."
-    }
-    elseif ($i -lt 15) {
+    } 
+    elseif ($i -lt 10) {
         $activity = "Scanning $target For Open Ports..."
     }
-    elseif ($i -lt 20) {
+    elseif ($i -lt 14) {
         $activity = "Connecting To $target..."
     }
-    elseif ($i -lt 35) {
+    elseif ($i -lt 29) {
         $activity = "Running Privilege Escalation Attacks..."
     }
-    elseif ($i -lt 40) {
+    elseif ($i -lt 33) {
         $activity = "Reconnecting As Administrator..."
     }
-    elseif ($i -lt 50) {
+    elseif ($i -lt 42) {
         $activity = "Scanning Connected Hard Drives..."
     }
-    elseif ($i -lt 70) {
+    elseif ($i -lt 64) {
         $activity = "Transfering Data To Local Device..."
     }
-    elseif ($i -lt 75) {
+    elseif ($i -lt 68) {
         $activity = "Clearing Log Files..."
     }
     elseif ($i -lt 85) {
@@ -41,12 +50,14 @@ for ($i = 0; $i -le 100; $i++) {
     }
 
     Write-Progress -Activity $activity -Status "$i% Complete" -PercentComplete $i
-    Start-Sleep -Milliseconds 400  # Simulate work being done
+    Start-Sleep -Milliseconds 200  # Simulate work being done
 }
 
 # Completion message
+Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" -ForegroundColor DarkCyan
+Write-Host "       $target Server Has Been Wiped!" -ForegroundColor Yellow
+Write-Host "See C:\$target\HackedFiles\ To Retieve Files!" -ForegroundColor Yellow
+Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" -ForegroundColor DarkCyan
 Write-Host ""
-Write-Host "$target Server Has Been Wiped! See C:\$target\HackedFiles To Retieve Gathered Information!" 
 Write-Host ""
 Read-Host -Prompt "Good Bye!"
-
